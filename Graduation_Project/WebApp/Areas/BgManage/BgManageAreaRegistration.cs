3323ce1f75c0126ff51b9bdc9081f278bdc3326d
GuDu Ying <2272247436@qq.com>
2016-12-01 17:13:01 +0800

    Add Areas

---
+++ Graduation_Project/WebApp/Areas/BgManage/BgManageAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace WebApp.Areas.BgManage
+{
+    public class BgManageAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "BgManage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "BgManage_default",
+                "Bg/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "WebApp.Areas.BgManage.Controllers" }
+            );
+        }
+    }
+}
