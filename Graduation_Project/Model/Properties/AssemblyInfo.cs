588aebc7dd78b58257372868611d088d8aab2829
GuDu Ying <2272247436@qq.com>
2016-12-02 00:43:28 +0800

    增加数据模型和部分工具类

--- Graduation_Project/Model/Properties/AssemblyInfo.cs
+++ Graduation_Project/Model/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("3d86819b-29d7-4dda-8c1a-e863754fa89f")]
+[assembly: Guid("aa781f10-fa6a-4a7b-af56-eb64712b7d34")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
